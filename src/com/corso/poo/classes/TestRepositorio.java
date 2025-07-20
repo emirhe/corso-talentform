f003d18c142809f5a65dc7d67d9fe56bc8ef512c
emirhe <emir.hg@gmail.com>
2025-07-20 21:20:46 +0200

    "Creacion-de-test-class"

---
+++ src/com/corso/poo/classes/TestRepositorio.java
@@ -0,0 +1,11 @@
+package com.corso.poo.classes;
+
+public class TestRepositorio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// questo è un test
+
+	}
+
+}
