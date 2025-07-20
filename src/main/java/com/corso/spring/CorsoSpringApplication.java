9d6e264c0e78db7ea724fe28edce818beeebb571
emirhe <emir.hg@gmail.com>
2025-07-20 11:47:54 +0200

    Agrego-estructurabase-para-cursospring

---
+++ src/main/java/com/corso/spring/CorsoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.corso.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corso/spring/CorsoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corso.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
