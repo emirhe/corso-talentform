0a9928f38bf5e53d307efa24171d1ba1fa189602
emirhe <emir.hg@gmail.com>
2025-07-21 12:27:52 +0200

    Clean branch: dejando solo README.md

--- src/com/corso/poo/classes/MainCellulare.java
+++
@@ -1,9 +0,0 @@
-package com.corso.poo.classes;
-
-public class MainCellulare {
-
-	public static void main(String[] args) {
-
-	}
-
-}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module CorsoJava {
-	requires java.sql;
-}
